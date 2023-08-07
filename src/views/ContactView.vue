8d28abc4d5ec6f0d702b545604930916586b4c87
Dusan <dusan@dusan.com>
2023-08-07 13:42:38 +0200

    Upload informations

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,6 +5,7 @@
                 <h1>Contact</h1>
                 <p>Email: dusanstojanovic890@gmail.com</p>
                 <p>Phone number: 0612066494</p>
+                <p>GitHub account with mini projectes: https://github.com/DusanS93</p>
                 <p>LinkedIn: https://www.linkedin.com/in/dusan-stojanovic-2b817b21b/</p>
                 <p>Location: Vinca, Grocka (Belgrade)</p>
             </div>
--- src/views/SkillsView.vue
+++ src/views/SkillsView.vue
@@ -5,7 +5,7 @@
                 <h1>Skills</h1>
                 <p><strong>Advanced skills:</strong> JavaScript (ES6), HTML5, CSS3, PHP (oop an procedrual) and MySql databases</p>
                 <p><strong>Begginer skills:</strong> React JS, Vue JS, Angular, Svelte JS, Node JS, TypeScript, TailwindCSS, Bootstrap, jQuery, Laravel, GIT, AJAX, JSON, XML, Word Press, Web services</p>
-                <p><strong>English language:</strong> begginer</p>
+                <p><strong>English language:</strong> intermediate</p>
             </div>
         </div>
     </article>
